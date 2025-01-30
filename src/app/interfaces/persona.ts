54a34d73e831280fc106df07ba8821e5c69d45cd
antoniovizarraga <antonio.vizarraga@iesnervion.es>
2025-01-30 14:32:24 +0100

    Implementación y creación de Services
    Hay que implementar mejor el verbo Get. Mirar apuntes del profesor.

--- src/app/interfaces/persona.ts
+++ src/app/interfaces/persona.ts
@@ -1,2 +1,10 @@
 export interface Persona {
+    id: Number,
+    nombre: String,
+    apellidos: String,
+    telefono: String,
+    direccion: String,
+    foto: String,
+    fechaNacimiento: Date,
+    idDepartamento: Number
 }
